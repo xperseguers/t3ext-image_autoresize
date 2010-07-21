f7b53a6563734e743749a63e59efe84d7a7f29f2
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-21 15:38:15 +0000

    Fixed XClass in TYPO3 4.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/image_autoresize/trunk@35898 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,7 @@ if (!defined ('TYPO3_MODE')) {
 
 // Postprocessing of upload of files
 if (!version_compare(TYPO3_version, '4.4.99', '>')) {
-	$GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_extfilefunc.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'classes/class.ux_t3lib_extfilefunctions_hook.php';
+	$GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_extfilefunc.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'classes/class.ux_t3lib_extfilefunctions.php';
 }
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_extfilefunc.php']['processData'][] = 'EXT:' . $_EXTKEY . '/classes/class.user_t3lib_extfilefunctions_hook.php:user_t3lib_extFileFunctions_hook';
 
