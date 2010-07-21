18ef3e4d43cf00b04e37f09fe4c8fa93ed313e20
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-21 16:41:43 +0000

    Small refactoring with extension parameters and localization
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/image_autoresize/trunk@35901 735d13b6-9817-0410-8766-e36946ffe9aa

--- classes/class.user_t3lib_extfilefunctions_hook.php
+++ classes/class.user_t3lib_extfilefunctions_hook.php
@@ -43,13 +43,34 @@ if (!version_compare(TYPO3_version, '4.4.99', '>')) {
 class user_t3lib_extFileFunctions_hook implements t3lib_extFileFunctions_processDataHook {
 
 	/**
+	 * @var array
+	 */
+	protected $config;
+
+	/**
+	 * Default constructor.
+	 */
+	public function __construct() {
+		$this->config = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['image_autoresize']);
+
+		$this->config['directories'] = t3lib_div::trimExplode(',', $this->config['directories']);
+		$this->config['filetypes'] = t3lib_div::trimExplode(',', $this->config['filetypes']);
+		$this->config['threshold'] = $this->convertHumanSizeToBytes(trim($this->config['threshold']));
+
+			// Sanitize name of the directories
+		foreach ($this->config['directories'] as &$directory) {
+			$directory = rtrim($directory, '/') . '/';
+		}
+	}
+
+	/**
 	 * Post process upload of a picture and make sure it is not too big.
 	 *
-	 * @param	string		The action
-	 * @param	array		The parameter sent to the action handler
-	 * @param	array		The results of all calls to the action handler 
-	 * @param	t3lib_extFileFunctions		parent t3lib_extFileFunctions object
-	 * @return	void
+	 * @param string The action
+	 * @param array The parameter sent to the action handler
+	 * @param array The results of all calls to the action handler 
+	 * @param t3lib_extFileFunctions parent object
+	 * @return void
 	 */
 	public function processData_postProcessAction($action, array $cmdArr, array $result, t3lib_extFileFunctions $pObj) {
 		if ($action !== 'upload') {
@@ -60,14 +81,20 @@ class user_t3lib_extFileFunctions_hook implements t3lib_extFileFunctions_process
 			// Get the latest uploaded file name
 		$filename = array_pop($result);
 		$relFilename = substr($filename, strlen(PATH_site));
-
-			// Extract the file extension
 		$imgExt = strtolower(substr($filename, strrpos($filename, '.') + 1));
-		$maxImageSize = 300 * 1024; // 300 KB
-		$maxWidth = 800;
-		$maxHeight = 600;
 
-		if (($imgExt === 'jpg' || $imgExt === 'jpeg') && filesize($filename) > $maxImageSize) {
+		$processFile = FALSE;
+		foreach ($this->config['directories'] as $directory) {
+			$processFile |= t3lib_div::isFirstPartOfStr($relFilename, $directory);
+		}
+		$processFile &= t3lib_div::inArray($this->config['filetypes'], $imgExt);
+
+		if (!$processFile) {
+				// Early return: directory and/or file type not configured
+			return;
+		}
+
+		if (filesize($filename) > $this->config['threshold']) {
 				// Image is bigger than allowed, will now resize it to (hopefully) make it lighter
 			$gifCreator = t3lib_div::makeInstance('tslib_gifbuilder');
 			$gifCreator->init();
@@ -76,8 +103,8 @@ class user_t3lib_extFileFunctions_hook implements t3lib_extFileFunctions_process
 			$hash = t3lib_div::shortMD5($filename);
 			$dest = $gifCreator->tempPath . $hash . '.' . $imgExt;
 			$options = array(
-				'maxW' => $maxWidth,
-				'maxH' => $maxHeight,
+				'maxW' => $this->config['maxWidth'],
+				'maxH' => $this->config['maxHeight'],
 			);
 
 			$tempFileInfo = $gifCreator->imageMagickConvert($filename, '', '', '', '', '', $options);
@@ -86,17 +113,47 @@ class user_t3lib_extFileFunctions_hook implements t3lib_extFileFunctions_process
 				@unlink($filename);
 				@rename($tempFileInfo[3], $filename);
 
-				$flashMessage = t3lib_div::makeInstance(
-					't3lib_FlashMessage',
+				$this->notify(
 					sprintf('Image %s has been automatically resized to %sx%s pixels', $relFilename, $tempFileInfo[0], $tempFileInfo[1]),
-					'',
-					t3lib_FlashMessage::INFO,
-					TRUE
+					t3lib_FlashMessage::INFO
 				);
-				t3lib_FlashMessageQueue::addMessage($flashMessage);
 			}
 		}
-	}	
+	}
+
+	/**
+	 * Converts a human file size into bytes.
+	 *
+	 * @param string $size
+	 * @return integer
+	 */
+	protected function convertHumanSizeToBytes($size) {
+		if (!is_numeric($size)) {
+			$unit = strtoupper(substr($size, -1));
+			$factor = 1 * ($unit === 'K' ? 1024 : ($unit === 'M' ? 1024 * 1024 : 0));
+			$size = intval(trim(substr($size, 0, strlen($size) - 1))) * $factor;
+		}
+		return $size;
+	}
+
+	/**
+	 * Notifies the user using a Flash message.
+	 *
+	 * @param string $message The message
+	 * @param integer $severity Optional severity, must be either of t3lib_FlashMessage::INFO, t3lib_FlashMessage::OK,
+	 *                          t3lib_FlashMessage::WARNING or t3lib_FlashMessage::ERROR. Default is t3lib_FlashMessage::OK.
+	 * @return void
+	 */
+	protected function notify($message, $severity = t3lib_FlashMessage::OK) {
+		$flashMessage = t3lib_div::makeInstance(
+			't3lib_FlashMessage',
+			$message,
+			'',
+			$severity,
+			TRUE
+		);
+		t3lib_FlashMessageQueue::addMessage($flashMessage);
+	}
 }
 
 
