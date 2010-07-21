c3bd9a08ef567eb4acae59267daa62aa2d778d66
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-21 19:14:10 +0000

    Updated checksums
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/image_autoresize/trunk@35907 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "image_autoresize"
+# Extension Manager/Repository config file for ext "image_autoresize".
 #
-# Auto generated 21-07-2010 17:08
+# Auto generated 21-07-2010 21:13
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -32,15 +32,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.0-',
-			'typo3' => '4.3.0-',
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '4.3.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => '',
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"ca41";s:21:"ext_conf_template.txt";s:4:"be72";s:12:"ext_icon.gif";s:4:"5cae";s:17:"ext_localconf.php";s:4:"3dd4";s:13:"locallang.xml";s:4:"adf0";s:50:"classes/class.user_t3lib_extfilefunctions_hook.php";s:4:"9ed5";s:43:"classes/class.ux_t3lib_extfilefunctions.php";s:4:"1810";s:63:"interfaces/interface.t3lib_extfilefunctions_processdatahook.php";s:4:"fc80";}',
 	'suggests' => array(
 	),
 );
