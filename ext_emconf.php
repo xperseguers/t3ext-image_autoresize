74e6dc93826035709966b6cf0d30556f89f328f6
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-18 10:52:54 +0000

    [TASK] Update md5 checksums
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/image_autoresize/trunk@59320 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "image_autoresize".
 #
-# Auto generated 07-11-2011 17:43
+# Auto generated 18-03-2012 11:52
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"6b04";s:12:"ext_icon.gif";s:4:"5cae";s:17:"ext_localconf.php";s:4:"910d";s:14:"ext_tables.php";s:4:"685f";s:12:"flexform.xml";s:4:"f9e9";s:13:"locallang.xml";s:4:"8333";s:26:"locallang_csh_flexform.xml";s:4:"f16f";s:36:"locallang_csh_tx_imageautoresize.xml";s:4:"197d";s:17:"locallang_tca.xml";s:4:"2ab2";s:7:"tca.php";s:4:"ef6b";s:39:"classes/class.user_fileupload_hooks.php";s:4:"c8d0";s:43:"classes/class.ux_t3lib_extfilefunctions.php";s:4:"fe40";s:34:"classes/class.ux_t3lib_tcemain.php";s:4:"d7be";s:14:"doc/manual.sxw";s:4:"f24c";s:63:"interfaces/interface.t3lib_extfilefunctions_processdatahook.php";s:4:"eb05";s:56:"interfaces/interface.t3lib_tcemain_processuploadhook.php";s:4:"3ae1";s:13:"mod1/conf.php";s:4:"198a";s:14:"mod1/index.php";s:4:"23e3";s:18:"mod1/locallang.xml";s:4:"26e3";s:22:"mod1/locallang_mod.xml";s:4:"705f";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"be13";s:12:"ext_icon.gif";s:4:"5cae";s:17:"ext_localconf.php";s:4:"910d";s:14:"ext_tables.php";s:4:"685f";s:12:"flexform.xml";s:4:"f9e9";s:13:"locallang.xml";s:4:"8333";s:26:"locallang_csh_flexform.xml";s:4:"f16f";s:36:"locallang_csh_tx_imageautoresize.xml";s:4:"197d";s:17:"locallang_tca.xml";s:4:"2ab2";s:7:"tca.php";s:4:"ef6b";s:39:"classes/class.user_fileupload_hooks.php";s:4:"c8d0";s:43:"classes/class.ux_t3lib_extfilefunctions.php";s:4:"fe40";s:34:"classes/class.ux_t3lib_tcemain.php";s:4:"d7be";s:14:"doc/manual.sxw";s:4:"f24c";s:63:"interfaces/interface.t3lib_extfilefunctions_processdatahook.php";s:4:"eb05";s:56:"interfaces/interface.t3lib_tcemain_processuploadhook.php";s:4:"3ae1";s:13:"mod1/conf.php";s:4:"198a";s:14:"mod1/index.php";s:4:"2009";s:18:"mod1/locallang.xml";s:4:"26e3";s:22:"mod1/locallang_mod.xml";s:4:"705f";}',
 	'suggests' => array(
 	),
 );
