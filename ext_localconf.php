a654b2055eb6bad796858bcd466905282ce26662
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-15 12:29:47 +0000

    [BUGFIX] Incorrect hook registration in TYPO3 < 6.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/image_autoresize/trunk@72967 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -14,9 +14,9 @@ if (version_compare(TYPO3_version, '6.0.0', '>=')) {
 	// Uploads in fileadmin/
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_extfilefunc.php']['processData'][] = 'EXT:' . $_EXTKEY . '/Classes/v4/class.user_fileupload_hooks.php:user_fileUpload_hooks';
 	// Uploads in uploads/
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processUpload'][] = 'EXT:' . $_EXTKEY . 'Classes/v4/class.user_fileupload_hooks.php:user_fileUpload_hooks';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processUpload'][] = 'EXT:' . $_EXTKEY . '/Classes/v4/class.user_fileupload_hooks.php:user_fileUpload_hooks';
 	// Uploads when using DAM
-	$TYPO3_CONF_VARS['EXTCONF']['dam']['fileTriggerClasses'][] = 'EXT:' . $_EXTKEY . 'Classes/v4/class.user_fileupload_hooks.php:user_fileUpload_hooks';
+	$TYPO3_CONF_VARS['EXTCONF']['dam']['fileTriggerClasses'][] = 'EXT:' . $_EXTKEY . '/Classes/v4/class.user_fileupload_hooks.php:user_fileUpload_hooks';
 }
 
 ?>
