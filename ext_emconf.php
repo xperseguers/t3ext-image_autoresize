40bcfa66a06a32d9b112f03c12ba6f90ab66490f
Xavier Perseguers <xavier@typo3.org>
2013-11-13 15:13:21 +0100

    [TASK] Check compatibility with PHP 5.5
    Successfully tested with:
    
    PHP 5.5.5-1~dotdeb.1 (cli) (built: Nov  3 2013 21:25:23)
    Copyright (c) 1997-2013 The PHP Group
    Zend Engine v2.5.0, Copyright (c) 1998-2013 Zend Technologies
        with Zend OPcache v7.0.3-dev, Copyright (c) 1999-2013, by Zend Technologies
    
    Change-Id: I084a0e542c8ad7d2e015fbae7ed3cb66dd94d046
    Reviewed-on: https://review.typo3.org/25379
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.4.0-dev',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.3-5.4.99',
+			'php' => '5.3.3-5.5.99',
 			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
