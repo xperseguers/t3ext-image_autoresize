fde69a16b0a367964336074348c2b1e4a9da5930
Xavier Perseguers <xavier@typo3.org>
2014-09-09 12:00:29 +0200

    [TASK] Use high-resolution module icon
    Change-Id: I64b7d87b5f296dc8fde190e0dcbf61e3eff02e25
    Reviewed-on: http://review.typo3.org/32669
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- mod1/conf.php
+++ mod1/conf.php
@@ -4,7 +4,7 @@ if (version_compare(TYPO3_branch, '6.2', '>=')) {
 } else {
 	$MCONF['name'] = 'tools_tximageautoresizeM1';
 	$MCONF['access'] = 'admin';
-	$MLANG['default']['tabs_images']['tab'] = '../ext_icon.gif';
+	$MLANG['default']['tabs_images']['tab'] = '../ext_icon.png';
 	$MLANG['default']['ll_ref'] = 'LLL:EXT:image_autoresize/mod1/locallang_mod.xml';
 }
 $MCONF['script'] = '_DISPATCH';
