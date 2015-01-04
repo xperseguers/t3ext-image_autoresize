035e9fb17a2b556208cade6723ec8cd9a598d1d3
Xavier Perseguers <xavier@typo3.org>
2015-01-04 16:12:02 +0100

    [TASK] Adapt copyright notice
    Change-Id: I6795274686b6239629e547ab4a15efbbdff159c3
    Reviewed-on: http://review.typo3.org/35778
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- Classes/Utility/JpegExifOrient.php
+++ Classes/Utility/JpegExifOrient.php
@@ -1,32 +1,20 @@
 <?php
 namespace Causal\ImageAutoresize\Utility;
 
-/***************************************************************
- *  Copyright notice
+/*
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2013-2014 Xavier Perseguers <xavier@causal.ch>
- *  based on work http://sylvana.net/jpegcrop/jpegexiforient.c
- *  All rights reserved
+ * It is based on work http://sylvana.net/jpegcrop/jpegexiforient.c
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *  A copy is found in the textfile GPL.txt and important notices to the license
- *  from the author is found in LICENSE.txt distributed with these scripts.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with TYPO3 source code.
  *
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
 /**
  * This is a utility class to set the Exif Orientation Tag.
