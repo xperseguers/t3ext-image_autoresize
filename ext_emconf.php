b0f94fcddea49ffa109eec0f21e8b5e66f4f6e4a
Xavier Perseguers <xavier@typo3.org>
2017-02-24 11:02:09 +0100

    [TASK] Raise version to 1.8.0-dev
    Change-Id: I8588852187006ba1c4955a0751218d06e81c3c0d
    Reviewed-on: https://review.typo3.org/51823
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,10 +30,10 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 0,
     'lockType' => '',
     'author_company' => 'Causal Sàrl',
-    'version' => '1.7.6-dev',
+    'version' => '1.8.0-dev',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.5.0-7.0.99',
+            'php' => '5.5.0-7.1.99',
             'typo3' => '6.2.0-8.99.99',
         ),
         'conflicts' => array(),
