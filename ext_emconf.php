65658612a4e3dfd915537b096bb3e07d7a273dea
Xavier Perseguers <xavier@causal.ch>
2020-03-05 07:14:06 +0100

    [TASK] Raise PHP compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.9.2-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.0.0-7.3.99',
+            'php' => '7.0.0-7.4.99',
             'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [],
