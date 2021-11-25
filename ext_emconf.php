3f8e47be73f0e2961a1e30e5860d3fe57ce1c923
Xavier Perseguers <xavier@causal.ch>
2021-11-25 09:10:50 +0100

    [TASK] Raise compatibility with PHP 8.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.1',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-8.0.99',
+            'php' => '7.2.0-8.1.99',
             'typo3' => '8.7.0-11.5.99',
         ],
         'conflicts' => [],
