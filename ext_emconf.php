067e1e9cf54288acfaa9720e0af700d451a6f967
Xavier Perseguers <xavier@causal.ch>
2022-10-21 11:55:34 +0200

    [TASK] Kickstart compatibility with TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,11 +22,11 @@ $EM_CONF[$_EXTKEY] = [
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
     'author_company' => 'Causal Sàrl',
-    'version' => '2.1.1',
+    'version' => '2.2-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-8.1.99',
-            'typo3' => '8.7.0-11.5.99',
+            'php' => '7.4.0-8.1.99',
+            'typo3' => '10.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
